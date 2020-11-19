b5b6640cbd506d7803e3aaf39da218405cad961a
Stefan de Jong <stefan_de_jong@icloud.com>
2020-11-19 11:33:20 +0100

    Added 8_bit_adder and changed alu

---
+++ 8_bit_or.vhd
@@ -0,0 +1,22 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+entity eight_bit_or is
+    port (
+        a : IN std_logic_vector(7 downto 0);
+        b : IN std_logic_vector(7 downto 0);
+        o : OUT std_logic_vector(7 downto 0)
+    );
+end entity;
+
+architecture behavioural of eight_bit_or is
+    o(0) <= a(0) or b (0);
+    o(1) <= a(1) or b (1);
+    o(2) <= a(2) or b (2);
+    o(3) <= a(3) or b (3);
+    o(4) <= a(4) or b (4);
+    o(5) <= a(5) or b (5);
+    o(6) <= a(6) or b (6);
+    o(7) <= a(7) or b (7);
+end behavioural;
--- alu.vhd
+++ alu.vhd
@@ -4,12 +4,25 @@ library ieee;
 
 entity alu is
   port (
-  a : IN std_logic_vector(7 downto 0);
-  b : IN std_logic_vector(7 downto 0);
-  controll : IN std_logic_vector(2 downto 0);
-  o : OUT std_logic_vector(7 downto 0)
+  a :         IN std_logic_vector(7 downto 0);
+  b :         IN std_logic_vector(7 downto 0);
+  controll :  IN std_logic_vector(2 downto 0);
+  o :         OUT std_logic_vector(7 downto 0);
+  AVR :       OUT std_logic;
+  ACR :       OUT std_logic;
+  HC  :       OUT std_logic
   );
 end entity;
+
+component full_ader is
+  port (
+    a : IN std_logic;
+    b : IN std_logic;
+    cin : IN std_logic;
+    o : OUT std_logic;
+    carry : OUT std_logic;
+  )
+end component
 
 -- Main operations
 -- Additinos, Or, Xor, And, Shift right, Carry in
@@ -29,25 +42,11 @@ begin
 
   -- Or
   when "001" =>
-  o(0) <= a(0) or b (0);
-  o(1) <= a(1) or b (1);
-  o(2) <= a(2) or b (2);
-  o(3) <= a(3) or b (3);
-  o(4) <= a(4) or b (4);
-  o(5) <= a(5) or b (5);
-  o(6) <= a(6) or b (6);
-  o(7) <= a(7) or b (7);
+
 
   -- Xor
   when "010" =>
-    o(0) <= a(0) xor b (0);
-    o(1) <= a(1) xor b (1);
-    o(2) <= a(2) xor b (2);
-    o(3) <= a(3) xor b (3);
-    o(4) <= a(4) xor b (4);
-    o(5) <= a(5) xor b (5);
-    o(6) <= a(6) xor b (6);
-    o(7) <= a(7) xor b (7);
+ 
 
   -- And
   when "011" =>
