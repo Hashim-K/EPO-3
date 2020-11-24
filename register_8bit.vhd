c0bb779fb85e72d755b7cef08562f8d3f3f053d2
Stefan de Jong <stefan_de_jong@icloud.com>
2020-11-24 15:37:40 +0100

    Update register_8bit.vhd

--- register_8bit.vhd
+++ register_8bit.vhd
@@ -1,41 +1,33 @@
-library IEEE;
-use IEEE.std_logic_1164.ALL;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
 
-entity register_8bit is
-   port(clk     : in  std_logic;
-        load    : in  std_logic;
-        reset   : in  std_logic;
-        data_in : in  std_logic_vector(7 downto 0);
-        write   : in  std_logic;
-        reg_out : out std_logic_vector(7 downto 0));
-end register_8bit;
+ENTITY register_8bit IS
+	PORT (
+		clk : IN STD_LOGIC;
+		load : IN STD_LOGIC;
+		reset : IN STD_LOGIC;
+		data_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+		reg_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
+END register_8bit;
 
-library IEEE;
-use IEEE.std_logic_1164.ALL;
-use IEEE.numeric_std.ALL;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
 
-architecture behaviour of register_8bit is
+ARCHITECTURE behaviour OF register_8bit IS
 
-signal q : std_logic_vector (7 downto 0);	--adding intermediate signal for output register
+	SIGNAL q : STD_LOGIC_VECTOR (7 DOWNTO 0); --adding intermediate signal for output register
 
-begin
-reg: process(clk, reset, load)	--process to determine output register
-		begin
-			if (rising_edge(clk) and load='1') then	--both need to be high to load value from bus
-				if (reset='1') then
-					q <= "00000000";		--clears the value in q
-				elsif (reset='0') then
-					q <= data_in;			--data from bus stored in q
-				end if;
-			end if;
-		end process;
-
-output: process(write)		--process to determine when to write to component/bus
-		begin
-			if (write='1') then
-				reg_out <= q;		-- only output when control tells to write, else high impedance
-			elsif (write='0') then
-				reg_out <= "ZZZZZZZZ";
-			end if;
-		end process;
-end behaviour;
+BEGIN
+	PROCESS (clk, reset, load) --process to determine output register
+	BEGIN
+		IF (rising_edge(clk) AND load = '1') THEN --both need to be high to load value from bus
+			IF (reset = '1') THEN
+				q <= "00000000"; --clears the value in q
+			ELSIF (reset = '0') THEN
+				q <= data_in; --data from bus stored in q
+			END IF;
+		END IF;
+	END PROCESS;
+	reg_out <= q;
+END behaviour;
