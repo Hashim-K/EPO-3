d823d393cfde9cd18d1e20292a1ad0b649cc934a
T0m6502 <74354775+T0m6502@users.noreply.github.com>
2020-11-24 14:59:15 +0100

    file added

---
+++ Ainputreg.vhdl
@@ -0,0 +1,15 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+entity a_input_register is
+  port (
+  clock :
+  );
+end entity;
+
+architecture arch of a_input_register is
+
+begin
+
+end architecture;
---
+++ Binputreg.vhdl
@@ -0,0 +1,15 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+entity B_input_register is
+  port (
+  clock
+  );
+end entity;
+
+architecture arch of B_input_register is
+
+begin
+
+end architecture;
