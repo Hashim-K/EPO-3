b40467c29eab992fc49256e1990c114c2e0fafec
Tom <T.o.geerling@student.tudelft.nl>
2020-12-01 14:03:58 +0100

    .

--- program_counter.vhdl
+++ program_counter.vhdl
@@ -6,13 +6,13 @@ entity program_counter is
   port (
   clk : IN std_logic;
   reset : IN std_logic;
-  I/PC : IN std_logic; -- Program counter increment enable
-  -- PCL/PCL : IN std_logic; -- Load form program counter I dont think this realy neaded
+  I_PC : IN std_logic; -- Program counter increment enable
+  -- PCL_PCL : IN std_logic; -- Load form program counter I dont think this realy neaded
   PCLC : OUT std_logic; -- carry out
   -- Bus controll signals
-  ADL/PCL : IN std_logic; -- load form Adress bus
-  PCL/DB  : IN std_logic; -- Put data on databus
-  PCL/ADL : IN std_logic; -- Put data on adress bus
+  ADL_PCL : IN std_logic; -- load form Adress bus
+  PCL_DB  : IN std_logic; -- Put data on databus
+  PCL_ADL : IN std_logic; -- Put data on adress bus
   -- Bus in
   ADB_IN : IN std_logic_vector(7 downto 0); -- addres buss in for loading
   -- Bus out
@@ -21,16 +21,20 @@ entity program_counter is
   );
 end entity;
 
-component register_8bit IS
-	PORT (
-		clk : IN STD_LOGIC;
-		load : IN STD_LOGIC;
-		reset : IN STD_LOGIC;
-		data_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-		reg_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
-END component;
+
 
 architecture arch of program_counter is
+
+  component register_8bit IS
+  	PORT (
+  		clk : IN STD_LOGIC;
+  		load : IN STD_LOGIC;
+  		reset : IN STD_LOGIC;
+  		data_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+  		reg_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
+  END component;
+
+
 signal load : std_logic;
 signal data_in, reg_out : std_logic_vector(7 downto 0);
 
@@ -38,31 +42,32 @@ signal controll : std_logic_vector(1 downto 0);
 
 signal to_increment, to_register : std_logic_vector(7 downto 0);
 begin
-  -- controll(3) <= PCL/PCL;
+  -- controll(3) <= PCL_PCL;
 
   -- Program counter select
-  with ADL/PCL select to_increment <=
-	ADB_IN when "1",
+  with ADL_PCL select to_increment <=
+	ADB_IN when '1',
   reg_out when others;
 
   -- Increment
-  with I/PC select to_register <=
-  std_logic_vector( unsigned(to_increment) + 1 ) when "1",
+  with I_PC select to_register <=
+  std_logic_vector( unsigned(to_increment) + 1 ) when '1',
   to_increment when others;
 
   -- register
-  l1 : register_8bit PORT MAP(clk,reset, load, to_register, reg_out);
+  l1 : register_8bit PORT MAP(clk, reset, load, to_register, reg_out);
+
 
 
   -- Adress bus output
-  with PCL/ADL select ADB_OUT <=
-  reg_out when "1",
-  "zzzzzzzz" when others;
+  with PCL_ADL select ADB_OUT <=
+  reg_out when '1',
+  "ZZZZZZZZ" when others;
 
   -- data bus output
-  with PCL/DB select DB_OUT <=
-  reg_out when "1",
-  "zzzzzzzz" when others;
+  with PCL_DB select DB_OUT <=
+  reg_out when '1',
+  "ZZZZZZZZ" when others;
 
 
 
--- testbench/program_counter_tb.vhdl
+++ testbench/program_counter_tb.vhdl
@@ -11,13 +11,13 @@ architecture arch of program_counter_tb is
     port (
     clk : IN std_logic;
     reset : IN std_logic;
-    I/PC : IN std_logic; -- Program counter increment enable
-    -- PCL/PCL : IN std_logic; -- Load form program counter I dont think this realy neaded
+    I_PC : IN std_logic; -- Program counter increment enable
+    -- PCL_PCL : IN std_logic; -- Load form program counter I dont think this realy neaded
     PCLC : OUT std_logic; -- carry out
     -- Bus controll signals
-    ADL/PCL : IN std_logic; -- load form Adress bus
-    PCL/DB  : IN std_logic; -- Put data on databus
-    PCL/ADL : IN std_logic; -- Put data on adress bus
+    ADL_PCL : IN std_logic; -- load form Adress bus
+    PCL_DB  : IN std_logic; -- Put data on databus
+    PCL_ADL : IN std_logic; -- Put data on adress bus
     -- Bus in
     ADB_IN : IN std_logic_vector(7 downto 0); -- addres buss in for loading
     -- Bus out
@@ -26,7 +26,7 @@ architecture arch of program_counter_tb is
     );
   end component;
 
-  signal I/PC, PCLC, ADL/PCL, PCL/DB, PCL/ADL : std_logic;
+  signal reset, I_PC, PCLC, ADL_PCL, PCL_DB, PCL_ADL : std_logic;
 
   signal ADB_IN, ADB_OUT, DB_OUT : std_logic_vector(7 downto 0);
 
@@ -41,14 +41,17 @@ begin
 
   ADB_IN <= "00000000";
 
+  I_PC <= '1';
 
-  ADL/PCL <= '0' after 0 ns,
-             '1' after 30 ns;
+
+  ADL_PCL <= '0' after 0 ns,
+             '1' after 30 ns,
              '0' after 40 ns;
-             
-  PCL/DB  <= '1' after 0 ns;
-  PCL/ADL <= '1' after 0 ns;
 
-  L1: program_counter PORT MAP(clk, reset, I/PC, PCLC, ADL/PCL, PCL/DB, PCL/ADL, ADB_IN, DB_OUT, ADB_OUT);
+  PCL_DB  <= '1' after 0 ns;
+
+  PCL_ADL <= '1' after 0 ns;
+
+  L1: program_counter PORT MAP(clk, reset, I_PC, PCLC, ADL_PCL, PCL_DB, PCL_ADL, ADB_IN, DB_OUT, ADB_OUT);
 
 end architecture;
