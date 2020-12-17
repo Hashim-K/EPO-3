f39e192ee04c8c8e487cbcd44ba32f37818d1c0a
Alex <ahirsig@outlook.com>
2020-12-17 09:56:24 +0100

    The synthesized instruction register

---
+++ Control/InstructionRegister/intruction_reg_behaviour_cfg.vhd
@@ -0,0 +1,4 @@
+configuration intruction_reg_behaviour_cfg of intruction_reg is
+   for behaviour
+   end for;
+end intruction_reg_behaviour_cfg;
---
+++ Control/InstructionRegister/intruction_reg_tb.vhd
@@ -0,0 +1,6 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+
+entity intruction_reg_tb is
+end intruction_reg_tb;
+
