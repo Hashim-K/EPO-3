e4a0ff23b67ba4dea9278865e47411b30ad84e5b
ahirsig <66413175+ahirsig@users.noreply.github.com>
2020-12-22 17:43:07 +0100

    the synthesized instruction register

---
+++ Control/InstructionRegister/intruction_reg_tb_behaviour_cfg.vhd
@@ -0,0 +1,6 @@
+configuration intruction_reg_tb_behaviour_cfg of intruction_reg_tb is
+   for behaviour
+      for all: intruction_reg use configuration work.intruction_reg_behaviour_cfg;
+      end for;
+   end for;
+end intruction_reg_tb_behaviour_cfg;
