12d6d7201f8864ff236dcc20d6af1738bc843d33
Paaataties <jornkay@live.nl>
2020-12-29 20:15:25 +0100

    Made the Predecode for the cc = 10 and 01 cases, the 00 is a bit more work but that is probably finished tomorrow.

--- Control/predecode_logic.vhdl
+++ Control/predecode_logic.vhdl
@@ -27,23 +27,58 @@ begin
 
       ----------------------------------- cc = 01 --------------------------------------
       if (databus(1 DOWNTO 0) = "01") then
+        RMW <= '0';
         if (databus(4 DOWNTO 2) = "000") then -- (zero page, X)
+          cycles <= "110";
         elsif (databus(4 DOWNTO 2) = "001") then -- (zero page)
+          cycles <= "011";
         elsif (databus (4 DOWNTO 2) = "010") then -- (#immediate)
+          cycles <= "010";
         elsif (databus (4 DOWNTO 2) = "010") then -- (absolute)
-        elsif (databus (4 DOWNTO 2) = "010") then -- (zero page, Y)
-        elsif (databus (4 DOWNTO 2) = "010") then -- (zero page), X
+          cycles <= "100";
+        elsif (databus (4 DOWNTO 2) = "010") then -- (zero page), Y
+          cycles <= "110";
+        elsif (databus (4 DOWNTO 2) = "010") then -- zero page, X
+          cycles <= "100";
         elsif (databus (4 DOWNTO 2) = "010") then -- absolute, Y
+          cycles <= "101";
         elsif (databus (4 DOWNTO 2) = "010") then -- absolute, X
+          cycles <= "101";
         end if;
+
+      ----------------------------------- cc = 10 --------------------------------------
       elsif (databus(1 DOWNTO 0) = "10") then
-        if (databus(4 DOWNTO 2) = "000") then -- #immediate
-        elsif (databus(4 DOWNTO 2) = "001") then -- zero page
-        elsif (databus(4 DOWNTO 2) = "010") then -- accumulator
-        elsif (databus(4 DOWNTO 2) = "011") then --absolute
-        elsif (databus(4 DOWNTO 2) = "101") then -- zero page, X
-        elsif (databus(4 DOWNTO 2) = "111") then -- absolute, X
+        if (databus(7 DOWNTO 5) = "100" or databus(7 DOWNTO 5) = "101") then -- STX and LDX are not RMV-instructions so seperate statement
+          RMW <= '0'
+          if (databus(4 DOWNTO 2) = "000") then -- #immediate
+            cycles <= "010";
+          elsif (databus(4 DOWNTO 2) = "001") then -- zero page
+            cycles <= "011";
+          elsif (databus(4 DOWNTO 2) = "010") then -- accumulator
+            cycles <= "010"; -- non-existent for these two functions
+          elsif (databus(4 DOWNTO 2) = "011") then --absolute
+            cycles <= "100";
+          elsif (databus(4 DOWNTO 2) = "101") then -- zero page, X/Y
+            cycles <= "100";
+          elsif (databus(4 DOWNTO 2) = "111") then -- absolute, X/Y
+            cycles <= "101";
+          end if;
+        elsif (databus(4 DOWNTO 2) = "010") then -- accumulator, needs seperate statement because accumulator is not RMW
+          cycles <= "010";
+          RMW <= '0';
+        else -- this is for the RMW-instructions
+          RMW <= '1';
+          if (databus(4 DOWNTO 2) = "001") then -- zero page
+            cycles <= "101";
+          elsif (databus(4 DOWNTO 2) = "011") then -- absolute
+            cycles <= "110";
+          elsif (databus(4 DOWNTO 2) = "101") then -- zero page, X/Y
+            cycles <= "110";
+          elsif (databus(4 DOWNTO 2) = "111") then -- absolute, X/Y
+            cycles <= "111";
+          end if;
         end if;
+        ----------------------------------- cc = 00 --------------------------------------
       elsif (databus(1 DOWNTO 0) = "00") then
         if (databus(4 DOWNTO 2) = "000") then -- #immediate
         elsif (databus(4 DOWNTO 2) = "001") then -- zero page
