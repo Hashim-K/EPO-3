06e6a33dccc967947a07783470ab90d0a3a2a1a1
ricksteman <ricksteman@live.nl>
2021-01-04 19:51:59 +0100

    changed IR load, not yet tested

--- Control/InstructionRegister/instruction_reg.vhdl
+++ Control/InstructionRegister/instruction_reg.vhdl
@@ -2,25 +2,33 @@ library IEEE;
 use IEEE.std_logic_1164.ALL;
 
 entity intruction_reg is
-   port(clk      : in  std_logic;
+   port(clk      : in  std_logic; -- first phase clock
         reset    : in  std_logic;
-        load     : in  std_logic;
+        rdy      : in std_logic;
+        sync     : in  std_logic;
         data_in  : in  std_logic_vector(7 downto 0);
         data_out : out std_logic_vector(7 downto 0));
 end intruction_reg;
 
 architecture behaviour of intruction_reg is
+
+signal q    : std_logic_vector(7 downto 0);
+signal load : std_logic;
+
 begin
-	process (clk, reset)
-	begin
-		if (rising_edge(clk)) then
-		    if (reset = '1') then
-			       data_out <=	"00000000";
-		else
-        if (load = '1') then
-			       data_out <=	data_in;
-        end if;
-		 end if;
-		end if;
-	end process;
-end behaviour;
+--creating the load signal based on pg44 of hardware manual
+load <= (sync AND rdy);
+
+BEGIN
+	PROCESS (clk, reset, load)
+	BEGIN
+		IF (rising_edge(clk)) THEN
+			IF (reset = '1') THEN
+				q <= "00000000";
+			ELSIF (reset = '0') and (load = '1') THEN
+				q <= data_in;
+			END IF;
+		END IF;
+	END PROCESS;
+data_out <= q;
+END behaviour;
