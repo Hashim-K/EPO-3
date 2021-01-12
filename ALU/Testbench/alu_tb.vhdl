e4dca22b5a2c1bff0afa85b36e4b495efc2ad5aa
Stefan de Jong <stefan_de_jong@icloud.com>
2021-01-12 14:30:39 +0100

    Lot of shit

--- ALU/Testbench/alu_tb.vhdl
+++ ALU/Testbench/alu_tb.vhdl
@@ -7,87 +7,72 @@ END ENTITY;
 
 ARCHITECTURE structural OF alu_tb IS
 
-component clock is
-  port (
-  clk_25mhz : IN std_logic; -- External cock in
-  reset : IN std_logic;
-  clk : OUT std_logic;  -- first phase clock
-  clk_2 : OUT std_logic -- Second phase clock
-  );
-end component;
+  COMPONENT clock IS
+    PORT (
+      clk_25mhz : IN STD_LOGIC; -- External cock in
+      reset : IN STD_LOGIC;
+      clk : OUT STD_LOGIC; -- first phase clock
+      clk_2 : OUT STD_LOGIC -- Second phase clock
+    );
+  END COMPONENT;
 
+  COMPONENT alu IS
+    PORT (
+      clk : IN STD_LOGIC;
+      reset : IN STD_LOGIC;
+      adl_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0); -- addres bus low in
+      adl_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0); -- addres bus low out 
+      sb_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0); -- system bus in
+      sb_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0); -- system bus out
+      db_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0); -- data bus in
 
-component alu is
-  port (
-  clk : in std_logic;
-  reset : in std_logic;
+      -- ALU logic 
+      control : IN STD_LOGIC_VECTOR(11 DOWNTO 0); -- control signals for ALU
+      --bit(0) = daa, not used since decimal is not implemented
+      --bit(1) = i/addc (carry in)
+      --bit(2) = sums (add)
+      --bit(3) = ands (and)
+      --bit(4) = exors (exor)
+      --bit(5) = ors (or)
+      --bit(6) = srs (shift right)
+      --bit(7) = sls (shift left)
+      --bit(8) = rotate right
+      --bit(9) = rotate left
+      --bit(10) = pass1 (register a)
+      --bit(11) = pass2 (register b)
 
+      avr : OUT STD_LOGIC; -- overflow flag
+      acr : OUT STD_LOGIC; -- carry out flag
+      hc : OUT STD_LOGIC; -- half carry flag
 
-  adl_in : in std_logic_vector(7 downto 0); -- addres bus low
-  adl_out : out std_logic_vector(7 downto 0); -- addres bus low
+      -- adder hold register
+      clk_2 : IN STD_LOGIC; -- second phase clock, used as load signal
+      add_adl : IN STD_LOGIC; -- output to addres low bus
+      add_sb6 : IN STD_LOGIC; -- output to SB bus 0-6
+      add_sb7 : IN STD_LOGIC; -- output to SB bus 7
 
-  sb_in : in std_logic_vector(7 downto 0); -- data bus in
-  sb_out : out std_logic_vector(7 downto 0); -- data bus out
+      -- A input register
+      o_add : IN STD_LOGIC; --load all 0's
+      sb_add : IN STD_LOGIC; --load data from SB
 
-
-  -- control signals
-    -- alu logic in
-    control : IN std_logic_vector(11 downto 0); -- alu operation mode
-    --bit(0) = daa, not used atm since decimal
-    --bit(1) = i/addc or called carry in
-    --bit(2) = sums
-    --bit(3) = ands
-    --bit(4) = exors
-    --bit(5) = ors
-    --bit(6) = srs (lsr)
-    --bit(7) = sls (asl)
-    --bit(8) = rotate right
-    --bit(9) = rotate left
-    --bit(10) = pass1 (rega)
-    --bit(11) = pass2 (regb)
-
-    daa : in std_logic;     -- decimal enable
-    i_addc : in std_logic;  -- carry in
-    srs : in std_logic;     --
-
-    -- alu logic out
-    avr : out std_logic;    -- overflow
-    acr : out std_logic;    -- carry out
-    hc : out std_logic;     -- half carry
-
-    -- adder hold register
-    clk_2 : in std_logic;   -- clock phase two input
-    add_adl : in std_logic; -- put content to aderes low bus
-    add_sb6 : in std_logic; -- put content to SB bus 0-6
-    add_sb7 : in std_logic; -- put content to sb bus 7
-
-    -- A input register
-    o_add : IN std_logic;  -- Load zero
-    sb_add : IN std_logic; -- Load form SB
-
-    -- B input register
-    inv_db_add : IN std_logic; -- inverted in from DB
-    db_add : IN std_logic;     -- load from DB
-    adl_add : IN std_logic     -- load from ADL
-);
-end component;
-
+      -- B input register
+      inv_db_add : IN STD_LOGIC; -- load databus inverse
+      db_add : IN STD_LOGIC; -- load databus
+      adl_add : IN STD_LOGIC -- load addres line
+    );
+  END COMPONENT;
 
   -- Clock signals and reset
-  signal clk_25mhz : std_logic;
-  signal special_clk_reset, reset, clk, clk_2 : std_logic;
-
-
+  SIGNAL clk_25mhz : STD_LOGIC;
+  SIGNAL special_clk_reset, reset, clk, clk_2 : STD_LOGIC;
   -- connected busses
-  signal adl_in, adl_out, sb_in, sb_out : std_logic_vector(7 downto 0);
-
-
+  SIGNAL adl_in, adl_out, sb_in, sb_out, db : STD_LOGIC_VECTOR(7 DOWNTO 0);
   -- control signals
-  signal daa, i_addc, srs, add_adl, add_sb6, add_sb7, o_add, sb_add, inv_db_add, db_add, adl_add : std_logic;
-  signal control : std_logic_vector(9 downto 0);
+  SIGNAL add_adl, add_sb6, add_sb7, o_add, sb_add, inv_db_add, db_add, adl_add : STD_LOGIC;
+  SIGNAL control : STD_LOGIC_VECTOR(11 DOWNTO 0);
 
-  -- output signals : overflow, carry out, helf carry
-  signal avr, acr, hc : std_logic;
+  -- output signals : overflow, carry out, half carry
+  SIGNAL avr, acr, hc : STD_LOGIC;
 
 BEGIN
 
@@ -96,70 +81,71 @@ BEGIN
   clk_25mhz <= '0' AFTER 0 ns,
     '1' AFTER 20 ns WHEN clk_25mhz /= '1' ELSE
     '0' AFTER 20 ns;
-
-
   -- reset (Reset needs to be very long becaus of two phase clock)
-  reset <= '1' after 0 ns,
-           '0' after 100 ns;
+  reset <= '1' AFTER 0 ns,
+    '0' AFTER 100 ns;
 
-  special_clk_reset <= '1' after 0 ns,
-                       '0' after 30 ns;
+  special_clk_reset <= '1' AFTER 0 ns,
+    '0' AFTER 30 ns;
 
--- Bus inputs
-
+  -- Bus inputs
   -- addres bus low
-   adl_in <= "00000001";
+  adl_in <= "00001100"; -- 12
+  -- system bus in
+  sb_in <= "00101000"; -- 40
   -- data bus in
-   sb_in <= "10000000";
+  db <= "00001100"; -- 12
 
-
--- Controll signals from decoder
-  control <= "0000010000";
-
-
-  daa         <= '0'; -- decimal mode
-  i_addc      <= '0'; -- cary in
-  srs         <= '0'; -- ?
+  -- Controll signals from decoder
+  control <= "000000000000" AFTER 0 ns,
+    "000000000100" AFTER 200 ns, --should be 52, checked
+    "000000000110" AFTER 600 ns, --should be 53, checked
+    "000000001000" AFTER 1000 ns, --and, checked
+    "000000010000" AFTER 1400 ns, --exor, checked
+    "000000100000" AFTER 1800 ns, --or, checked
+    "000001000000" AFTER 2200 ns, --shift right, checked
+    "000010000000" AFTER 2600 ns, --shift left, checked
+    "000100000010" AFTER 3000 ns, --rotate right, checked
+    "001000000010" AFTER 3400 ns, --rotate left, checked
+    "010000000000" AFTER 3800 ns, --pass a, checked
+    "100000000000" AFTER 4200 ns; --pass b, checked
 
   -- adder hold register
-  add_adl     <= '0'; -- put content to aderes low bus
-  add_sb6     <= '1'; -- put content to SB bus 0-6
-  add_sb7     <= '1'; -- put content to sb bus 7
+  add_adl <= '0'; 
+  add_sb6 <= '1';
+  add_sb7 <= '1';
 
   -- A register control signals
-  o_add       <= '0'; -- Load zero
-  sb_add      <= '1'; -- Load form SB
+  o_add <= '0'; -- Load zero
+  sb_add <= '1'; -- Load from SB
 
   -- B register control signals
-  inv_db_add  <= '0'; -- inverted in from DB
-  db_add      <= '0'; -- load from DB
-  adl_add     <= '1'; -- load from ADL
+  inv_db_add <= '0';
+  db_add <= '0';
+  adl_add <= '1';
 
-
-
--- Alu big portmap
-  L1 : alu PORT MAP(clk,
-                    reset,
-                    adl_in,
-                    adl_out,
-                    sb_in,
-                    sb_out,
-                    control,
-                    daa,
-                    i_addc,
-                    srs,
-                    avr,
-                    acr,
-                    hc,
-                    clk_2,
-                    add_adl,
-                    add_sb6,
-                    add_sb7,
-                    o_add,
-                    sb_add,
-                    inv_db_add,
-                    db_add,
-                    adl_add
-);
+  -- Alu big portmap
+  L1 : alu PORT MAP(
+    clk,
+    reset,
+    adl_in,
+    adl_out,
+    sb_in,
+    sb_out,
+    db,
+    control,
+    avr,
+    acr,
+    hc,
+    clk_2,
+    add_adl,
+    add_sb6,
+    add_sb7,
+    o_add,
+    sb_add,
+    inv_db_add,
+    db_add,
+    adl_add
+  );
 
 END ARCHITECTURE;
--- ALU/adder_hold_register.vhdl
+++ ALU/adder_hold_register.vhdl
@@ -1,4 +1,3 @@
--- Verified working!! Tom
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
@@ -8,14 +7,14 @@ ENTITY adder_hold_register IS
     clk : IN STD_LOGIC;
     reset : IN STD_LOGIC;
 
-    alu_data_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0); -- 8 bit input from alu
+    alu_data_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0); -- input from alu
     adl : OUT STD_LOGIC_VECTOR(7 DOWNTO 0); -- addres low bus
     sb : OUT STD_LOGIC_VECTOR(7 DOWNTO 0); -- system bus
 
-    clk_2 : IN STD_LOGIC; -- load the content of the alu into register, this is connected to the second phase clock!
-    add_adl : IN STD_LOGIC; -- put content to aderes low bus
-    add_sb6 : IN STD_LOGIC; -- put content to SB bus 0-6
-    add_sb7 : IN STD_LOGIC -- put content to sb bus 7
+    clk_2 : IN STD_LOGIC; -- second phase clock, used as load signal
+    add_adl : IN STD_LOGIC; -- output to ADL
+    add_sb6 : IN STD_LOGIC; -- output to SB bus 0-6
+    add_sb7 : IN STD_LOGIC -- output to SB bus 7
   );
 END ENTITY;
 
@@ -48,8 +47,6 @@ BEGIN
     '0' & reg_out(6 DOWNTO 0) WHEN "01",
     reg_out(7) & "0000000" WHEN "10",
     "ZZZZZZZZ" WHEN OTHERS;
-
-
   l1 : register_8bit PORT MAP(clk_2, '1', reset, alu_data_in, reg_out);
 
 END ARCHITECTURE;
