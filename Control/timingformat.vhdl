aa57550aa7b8fa35ae174d8177e50a76d32d3b46
Hashim Karim <hashimkarim168@gmail.com>
2021-01-12 14:32:50 +0100

    Updated

--- Control/timingformat.vhdl
+++ Control/timingformat.vhdl
@@ -1,4 +1,21 @@
---Timing: T
-if tcstate()='0') then
-  control_out<="";
+--Timing: T2
+if (tcstate(2)='0') then
+end if;
+--Timing: T3
+if (tcstate(3)='0') then
+end if;
+--Timing: T4
+if (tcstate(4)='0') then
+end if;
+--Timing: T5
+if (tcstate(5)='0') then
+end if;
+--Timing: T6
+if (tcstate(0)='1'&tcstate(1)='1'&tcstate(2)='1'&tcstate(3)='1'&tcstate(4)='1'&tcstate(5)='1') then
+end if;
+--Timing: T0
+if (tcstate(0)='0') then
+end if;
+--Timing: T1
+if (tcstate(1)='0') then
 end if;
