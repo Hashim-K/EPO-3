5f410fe8d32bdd4f1f30651771a3e245ea0fb6b3
ricksteman <ricksteman@live.nl>
2021-01-14 19:57:08 +0100

    hopefully the fix to the synthesis problem

--- Mosfets/Pass Mosfets/pass_mosfet.vhdl
+++ Mosfets/Pass Mosfets/pass_mosfet.vhdl
@@ -2,17 +2,25 @@ library IEEE;
 use IEEE.std_logic_1164.ALL;
 
 entity pass is
-   port(buss_in   : in  std_logic_vector(7 downto 0);
-        enable_pass   : in  std_logic;
-        buss_out  : out std_logic_vector(7 downto 0)
+   port(bus_in_1   : in  std_logic_vector(7 downto 0);
+        bus_in_2   : in std_logic_vector(7 downto 0);
+        enable_pass: in  std_logic_vector(1 downto 0);
+        --enable_pass(0) db -> sb and adh -> sb
+        --enable_pass(1) sb -> db and sb -> adh
+        bus_out_1  : out std_logic_vector(7 downto 0);
+        bus_out_2  : out std_logic_vector(7 downto 0)
         );
 end pass;
 
 architecture behaviour of pass is
 begin
-   with enable_pass select buss_out <=
-	  buss_in when '1',
-	  "ZZZZZZZZ" when others;
 
+with enable_pass select bus_out_1 <= -- sb for both
+bus_in_1 when "01", -- db and adh
+"ZZZZZZZZ" when others;
+
+with enable_pass select bus_out_2 <= -- db and adh
+bus_in_2 when "10", -- sb for both
+"ZZZZZZZZ" when others;
 
 end behaviour;
--- Mosfets/Pass Mosfets/pass_mosfet_tb.vhdl
+++ Mosfets/Pass Mosfets/pass_mosfet_tb.vhdl
@@ -6,27 +6,28 @@ end pass_tb;
 
 architecture behaviour of pass_tb is
    component pass
-      port(buss_in   : inout  std_logic_vector(7 downto 0);
-           enable_pass   : in  std_logic;
-           buss_out  : inout std_logic_vector(7 downto 0));
+   port(bus_in_1   : in  std_logic_vector(7 downto 0);
+        bus_in_2   : in std_logic_vector(7 downto 0);
+        enable_pass: in  std_logic_vector(1 downto 0);
+        --enable_pass(0) db -> sb and adh -> sb
+        --enable_pass(1) sb -> db and sb -> adh
+        bus_out_1  : out std_logic_vector(7 downto 0);
+        bus_out_2  : out std_logic_vector(7 downto 0)
+        );
    end component;
 
 
-   signal buss_in   : std_logic_vector(7 downto 0);
-   signal enable_pass : std_logic;
-   signal buss_out  : std_logic_vector(7 downto 0);
+   signal bus_in_1, bus_in_2   : std_logic_vector(7 downto 0);
+   signal enable_pass : std_logic_vector(1 downto 0);
+   signal bus_out_1, bus_out_2  : std_logic_vector(7 downto 0);
 begin
-   test: pass port map (buss_in, buss_out, enable_pass);
+   test: pass port map (bus_in_1, bus_in_2, enable_pass, bus_out_1, bus_out_2);
 
-   buss_in <= "11111111" after 0 ns,
-              "XXXXXXXX" after 100 ns;
+   bus_in_1 <= "11111111" after 0 ns;
 
-   buss_out <="XXXXXXXX" after 0 ns,
-              "11111111" after 100 ns;
+   bus_in_2 <="00000000" after 0 ns;
 
-   enable_pass <= '0' after 0 ns,
-                  '1' after 20 ns,
-                  '0' after 40 ns,
-                  '1' after 100 ns,
-                  '0' after 150 ns;
+   enable_pass <= "00" after 0 ns,
+                  "01" after 50 ns,
+                  "10" after 100 ns;
 end behaviour;
